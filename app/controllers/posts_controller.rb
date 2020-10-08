f8888bc717f0da2181f9ff0481453b3f3c9f3816
edmundo papi <edmundopierre@yahoo.com>
2020-10-08 09:43:33 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+
   def show
     @post = Post.find(params[:id])
   end
@@ -12,4 +13,12 @@ class PostsController < ApplicationController
   end
 
   # add create method here
+  def create
+     post = Post.new
+     post.title = params[:title]
+   post.description = params[:description]
+   post.save
+   redirect_to posts_path(@post)
+
+  end
 end
